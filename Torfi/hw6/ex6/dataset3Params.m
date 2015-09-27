0b1d7da414c920a7622973d8a0442db9fc06359d
OlafurTorfi <olafurtorfi@gmail.com>
2015-09-27 15:59:27 +0000

    done

--- Torfi/hw6/ex6/dataset3Params.m
+++ Torfi/hw6/ex6/dataset3Params.m
@@ -2,27 +2,43 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
-
+C = 0.01;
+sigma = 0.005;
+temp_C = C;
+error = 1000000;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
-
+for i=1:10
+  temp_sigma = 0.005;
+  temp_C = temp_C * 3;
+  for j=1:10
+    temp_sigma = temp_sigma * 3;
+    predictions =svmPredict(@svmTrain(X, y, temp_C, @(x1,x2) gaussianKernel(x1,x2,temp_sigma)), Xval);
+    new_error = mean(double(predictions ~= yval));
+    if(new_error < error)
+      C = temp_C;
+      sigma = temp_sigma;
+      error = new_error;
+    end
+  end
+end
+C
+sigma
 
 
 
---
+++ Torfi/hw6/ex6/tests.m
@@ -0,0 +1,5 @@
+% test second part directly
+load('ex6data3.mat');
+
+% Try different SVM Parameters here
+[C, sigma] = dataset3Params(X, y, Xval, yval);
